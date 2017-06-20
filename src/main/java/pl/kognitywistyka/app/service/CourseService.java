fb04b24eab6818a8c5e00742111fbc24fa6de4ff
wikror <wiktor.rorot@gmail.com>
2017-06-20 20:24:47 +0200

    mock persistence for courses for developing frontend

--- src/main/java/pl/kognitywistyka/app/service/CourseService.java
+++ src/main/java/pl/kognitywistyka/app/service/CourseService.java
@@ -4,6 +4,7 @@ import pl.kognitywistyka.app.course.Course;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by wikto on 20.06.2017.
@@ -11,7 +12,14 @@ import java.util.List;
 public class CourseService {
 
     //todo
-    public static List<Course> findAllCourses() {
-        return new ArrayList<Course>();
+    public static List<Course> findAll() {
+        return new ArrayList<>();
+    }
+
+    public static List<Course> findAll(String value) {
+        return new ArrayList<>();
+    }
+
+    public static void register(Set<Course> selectedCourses) {
     }
 }
