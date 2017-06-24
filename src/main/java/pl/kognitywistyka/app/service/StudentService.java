8d9771238eb90c0bdbe6803e04664fcecc5a1032
wikror <wiktor.rorot@gmail.com>
2017-06-24 17:14:26 +0200

    StudentService queries

--- src/main/java/pl/kognitywistyka/app/service/StudentService.java
+++ src/main/java/pl/kognitywistyka/app/service/StudentService.java
@@ -45,11 +45,6 @@ public class StudentService {
         }
     }
 
-    //todo
-//    public synchronized List<User> findAll() {
-//        return new ArrayList<>();
-//    }
-
     public synchronized List<User> findById(String id) throws NoResultException {
         Session session = HibernateUtils.getSessionFactory().openSession();
         Transaction tx = null;
@@ -120,60 +115,87 @@ public class StudentService {
     }
 
     //Adds user to db
-    public void addUser(User user) {
+    public boolean add(User user) {
+        user.setPassword(AuthenticationService.sha512(user.getPassword()));
         Session session = HibernateUtils.getSessionFactory().openSession();
         Transaction tx = null;
         try {
             tx = session.beginTransaction();
+            //todo querying if exists?
             session.save(user);
-            tx.commit();
+            session.getTransaction().commit();
         } catch (Exception e) {
             if (tx != null) tx.rollback();
-            session.close();
             e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
         }
+        return true;
     }
 
-    public synchronized ArrayList<User> findAll(String value) {
-        ArrayList<User> arrayList = new ArrayList<>();
-        for (User user : users.values()) {
-            boolean passesFilter = (value == null || value.isEmpty())
-                    || user.toString().toLowerCase().contains(value.toLowerCase());
-            if (passesFilter) {
-                //todo we should implement ordering here
-                arrayList.add(user);
-            }
+    public boolean delete(User user) {
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            //todo querying if exists?
+            session.remove(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
         }
-//        Collections.sort(arrayList, (o1, o2) -> (int) ( - o1.getLocalId()));
-        return arrayList;
-    }
-
-    public void register(Set<Course> selectedCourses) {
-    }
-
-    public boolean delete(User student) {
-        users.remove(student.getId());
         return true;
     }
 
     public boolean delete(Set<User> selectedStudents) {
-        for (User user : selectedStudents) {
-            users.remove(user.getId());
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            for (User user : selectedStudents) {
+                session.remove(user);
+            }
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
         }
         return true;
     }
 
-    public boolean add(Student student) {
-        try {
-            student.setPassword(AuthenticationService.sha512(student.getPassword()));
-            if (!users.containsKey(student.getId())) {
-                users.put(student.getId(), student);
-                return true;
-            } else {
-                throw new Exception();
-            }
-        } catch (Exception e) {
-            return false;
-        }
-    }
+//    public synchronized ArrayList<User> findAll(String value) {
+//        ArrayList<User> arrayList = new ArrayList<>();
+//        for (User user : users.values()) {
+//            boolean passesFilter = (value == null || value.isEmpty())
+//                    || user.toString().toLowerCase().contains(value.toLowerCase());
+//            if (passesFilter) {
+//                //todo we should implement ordering here
+//                arrayList.add(user);
+//            }
+//        }
+////        Collections.sort(arrayList, (o1, o2) -> (int) ( - o1.getLocalId()));
+//        return arrayList;
+//    }
+
+//    public boolean add(Student student) {
+//        try {
+//            student.setPassword(AuthenticationService.sha512(student.getPassword()));
+//            if (!users.containsKey(student.getId())) {
+//                users.put(student.getId(), student);
+//                return true;
+//            } else {
+//                throw new Exception();
+//            }
+//        } catch (Exception e) {
+//            return false;
+//        }
+//    }
 }
